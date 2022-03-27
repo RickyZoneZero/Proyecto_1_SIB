08d4558bb66fbedefb33c408726bf683e874666f
Enrique <java.zone.in.out@gmail.com>
2022-03-26 21:49:37 -0600

    Haciendo modificaciones

--- register.php
+++ register.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
     $dbconection = pg_connect("host=192.168.100.193 port=25000 dbname=DB_Proyecto user=DB_Proyecto password=DB_Proyecto");       
 
@@ -21,7 +12,5 @@
 
     pg.close();
 
-    header('Location: error.html');
+    header('Location:error.html');
 ?>    
-</body>
-</html>
